eecff077b77fda9c3137aef226d1983c98cbbc63
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2017-10-03 15:34:06 +0300

    Create /static dir to place wasm build to.

--- kotlin-native/samples/fullstack/clients/webassembly/build.sh
+++ kotlin-native/samples/fullstack/clients/webassembly/build.sh
@@ -2,10 +2,12 @@
 
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )
 PATH=$KONAN_HOME/bin:$PATH
+STATIC_DIR=$DIR/../../static/
+mkdir -p $STATIC_DIR
 
 konanc StatView.kt \
     -r $DIR/../../../../external/klib \
     -l canvas -l jsinterop \
     -target wasm32 -o $DIR/../../static/view
 
-cp ./index.html $DIR/../../static/
+cp ./index.html $STATIC_DIR
