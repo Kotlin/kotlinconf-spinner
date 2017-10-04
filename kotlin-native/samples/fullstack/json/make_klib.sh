57bc5f15eb5b63ab62c0cb1afcdaf1ca15a8ddcd
Nikolay Igotti <igotti@gmail.com>
2017-10-05 00:25:06 +0300

    Android sample builds on Linux

--- kotlin-native/samples/fullstack/json/make_klib.sh
+++ kotlin-native/samples/fullstack/json/make_klib.sh
@@ -2,7 +2,21 @@
 
 PATH=$KONAN_HOME/bin:$PATH
 
-cinterop -def jansson.def -o jansson.klib -library ../common/common.klib
+if [ x$TARGET == x ]; then
+case "$OSTYPE" in
+  darwin*)  TARGET=osx ;;
+  linux*)   TARGET=linux ;;
+  *)        echo "unknown: $OSTYPE" && exit 1;;
+esac
+fi
+
+COMPILER_OPTS_linux="-I/usr/include"
+COMPILER_OPTS_osx="-I/opt/local/include"
+
+var=COMPILER_OPTS_${TARGET}
+COMPILER_OPTS="${!var}"
+
+cinterop -def jansson.def -o jansson.klib -library ../common/common.klib -compilerOpts "$COMPILER_OPTS"
 konanc -p library KJson.kt -o kjson.klib \
      -r ../common \
      -l jansson \
