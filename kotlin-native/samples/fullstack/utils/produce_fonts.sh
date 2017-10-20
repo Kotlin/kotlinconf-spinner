4a223c87f1294a9694bbf521163e8f5686ef658b
Ilya Matveev <ilya.matveev@jetbrains.com>
2017-10-20 19:01:36 +0300

    Add gradle build in utils

--- kotlin-native/samples/fullstack/utils/produce_fonts.sh
+++ kotlin-native/samples/fullstack/utils/produce_fonts.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 alphabet="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-androidFont="/Users/jetbrains/kotlin/kotlin/dependencies/androidSDK/platforms/android-25/data/fonts/Roboto-Black.ttf"
+androidFont="$ANDROID_HOME/platforms/android-25/data/fonts/Roboto-Black.ttf"
 iosFont="/System/Library/Fonts/Menlo.ttc"
 fontSize=100
 
