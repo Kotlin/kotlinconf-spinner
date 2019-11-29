b01a686e473a2757b9aa4b34689c68f79095ae38
Nikolay Igotti <igotti@gmail.com>
2019-11-29 09:29:08 +0300

    Automated beacon adding.

--- clients/cli/src/hostMain/kotlin/Client.kt
+++ clients/cli/src/hostMain/kotlin/Client.kt
@@ -1,6 +1,7 @@
 import kjson.*
 import kliopt.*
 import kommon.cityHash64
+import kommon.readFileData
 import kurl.*
 import kommon.machineName
 import kotlin.system.exitProcess
@@ -8,6 +9,29 @@ import kotlin.system.exitProcess
 fun showUsage(message: String) {
     println(message)
     exitProcess(1)
+}
+
+fun addAll(kurl: KUrl, url: String, urlParams: MutableMap<String, String>, filename: String) {
+    val data = readFileData(filename) ?: return
+    val contents = data.decodeToString()
+
+    contents.split('\n').forEach {
+        val line = it.split('|')
+        if (line.size == 5) {
+            val code = line[1]
+            val name = line[2]
+            val stringth = line[3]
+            val active = line[4]
+            urlParams["beacon"] = "$code,$name,$stringth,$active"
+            println("Sending ${urlParams["beacon"]}")
+            kurl.fetch(url, urlParams) { content ->
+                withJson(content) {
+                    println("Got $it")
+                }
+            }
+        }
+    }
+}
 }
 
 fun main(args: Array<String>) {
@@ -24,11 +48,14 @@ fun main(args: Array<String>) {
             OptionDescriptor(OptionType.STRING, "p", "password", "Password for admin access"),
 
             OptionDescriptor(OptionType.STRING, "f", "fcommand", "Finder command to issue"),
-            OptionDescriptor(OptionType.STRING, "P", "parameter", "Parameter to pass, in form key=value")
+            OptionDescriptor(OptionType.STRING, "P", "parameter", "Parameter to pass, in form key=value"),
+            OptionDescriptor(OptionType.STRING, "B", "beaconfile", "Name of file with beacon list")
+
     )
     var password = ""
     var parameterName = ""
     var parameterValue = ""
+    var beaconfile = ""
     parseOptions(options, args).forEach {
         when (it.descriptor?.longName) {
             "server" -> server = it.stringValue
@@ -51,6 +78,7 @@ fun main(args: Array<String>) {
                 }
             }
             "password" -> password = it.stringValue
+            "beaconfile" -> beaconfile = it.stringValue
             "help" -> showUsage(makeUsage(options))
         }
     }
@@ -59,7 +87,7 @@ fun main(args: Array<String>) {
     val url = "$server/$service/$command"
     val urlParams = mutableMapOf("name" to name, "client" to "cli", "machine" to machine, "password" to password)
     if (command == "proximity") {
-        // Replace device name with the hash.   println("p=$parameterValue")
+        // Replace device name with the hash.
         parameterValue = parameterValue.split(',').map {
             val deviceStrength = it.split(':').toMutableList()
             deviceStrength[0] = deviceStrength[0].cityHash64().toString(16)
@@ -69,9 +97,14 @@ fun main(args: Array<String>) {
     if (parameterName.isNotEmpty()) urlParams[parameterName] = parameterValue
     println("Connecting to $url as $name from $machine")
     println("passing parameters: $urlParams")
-    withUrl(kurl) { it.fetch(url, urlParams) {
-        content -> withJson(content) {
-                println("Got $it")
+    withUrl(kurl) {
+        if (beaconfile.isNotEmpty()) {
+            addAll(it, url, urlParams, beaconfile)
+        } else {
+            it.fetch(url, urlParams) { content ->
+                withJson(content) {
+                    println("Got $it")
+                }
             }
         }
     }
